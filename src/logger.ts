9cac3f448eb481ed723d801763e9f00d884a261d
David Nussio <david.nussio@gmail.com>
2022-11-30 10:08:52 +0100

    chore: support languageId in result view

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 
-export const Logger = vscode.window.createOutputChannel("jqpg");
+export const Logger = vscode.window.createOutputChannel("jqpg", "json");
 export const Debug = vscode.window.createOutputChannel("jqpg debug");
 
 export default Logger;
